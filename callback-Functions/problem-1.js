087ab3e4c86d8978fdccc3424099708af9bd48a6
Sudharsan-lumel <SudharsanH@lumel.com>
2025-07-12 19:27:08 +0530

    solve the callback functions sums

---
+++ callback-Functions/problem-1.js
@@ -0,0 +1,22 @@
+// Write a JavaScript program to find multiply, add for the given number using the callback
+
+function calculate(number, callback) {
+    return callback(number);
+  }
+
+  function add(num) {
+    return num + 10;
+  }
+  
+  function multiplyByFive(num) {
+    return num * 5;
+  }
+
+  let number = 4;
+  let addedResult = calculate(number, addTen);
+  let multipliedResult = calculate(number, multiplyByFive);
+  
+  console.log(`Original number: ${number}`);
+  console.log(`After addition: ${addedResult}`);
+  console.log(`After multiplication: ${multipliedResult}`);
+  
---
+++ callback-Functions/problem-2.js
@@ -0,0 +1,4 @@
+// 2.Write a JavaScript program to get input from user using prompt and print it using callback.
+
+
+let input=prompt('Hii how are y');
