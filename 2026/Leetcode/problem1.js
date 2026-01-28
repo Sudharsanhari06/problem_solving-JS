2eb2b9106896e9c72e4f8e2f812199103c727fdd
Sudharsanhari06 <sudharsanhari2006@gmail.com>
2026-01-28 22:56:07 +0530

    array: Minimum Cost chips sum

---
+++ 2026/Leetcode/problem1.js
@@ -0,0 +1,22 @@
+// 1217. Minimum Cost to Move Chips to The Same Position
+
+
+var minCostToMoveChips = function(position) {
+    let odd=0;
+    let even=0;
+
+    for(let item of position){
+        if(item%2){
+            odd++;
+        }else{
+            even++;
+        }
+    }
+
+return Math.min(odd,even);
+
+};
+let position = [1,2,3];
+let positionTwo = [2,2,2,3,3]
+console.log( minCostToMoveChips(position));
+console.log(minCostToMoveChips(positionTwo));
