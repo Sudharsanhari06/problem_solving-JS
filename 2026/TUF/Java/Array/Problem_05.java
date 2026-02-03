6ef6b1c0ef5ebaa8634926c1f73fbaf4b4c10feb
Sudharsanhari06 <sudharsanhari2006@gmail.com>
2026-02-03 22:57:38 +0530

    rotate by array

---
+++ 2026/TUF/Java/Array/Problem_05.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+class Problem_05 {
+
+    static int[] firstLeftArray(int[] arr){
+        int first=arr[0];
+
+        for(int i=0;i<arr.length-1;i++){
+            arr[i]=arr[i+1];
+        }
+        arr[arr.length-1]=first;    
+        return arr; 
+
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println("Left Rotate Array:"+ Arrays.toString(firstLeftArray(arr)));
+    }
+}
