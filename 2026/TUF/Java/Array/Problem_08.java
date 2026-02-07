2018c441f4561a527b1af02409a11c38f05a76bb
Sudharsanhari06 <sudharsanhari2006@gmail.com>
2026-02-07 22:34:52 +0530

    problem solving

--- 2026/TUF/Java/Array/Problem_08.java
+++ 2026/TUF/Java/Array/Problem_08.java
@@ -1,5 +1,7 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
+// Count Maximum Consecutive One's in the array
+
 
 class Problem_08 {
     
---
+++ 2026/TUF/Java/Array/Problem_09.java
@@ -0,0 +1,29 @@
+
+
+
+class Problem_09 {
+    
+    static int appearsOnce(int[] arr){
+ 
+        for(int i=0;i<arr.length;i++){
+            int num=arr[i];
+            int count=0;
+            for(int j=0;j<arr.length;j++){
+                if(num==arr[j]){
+                    count++;
+                }     
+            }
+            if(count==1){
+                return num;
+            }
+        }
+        return -1;
+        
+    }
+    
+    public static void main(String[] args) {
+        int arr[] ={4,1,2,1,2};
+
+        System.out.println("Find the number that appears once: "+appearsOnce(arr));
+    }
+}
